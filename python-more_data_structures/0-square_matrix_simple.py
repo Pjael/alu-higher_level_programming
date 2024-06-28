a5750f3255e82057202ecba8341bd8bcfc33d5b3
root <p.savadogo@alustudent.com>
2024-06-28 02:32:28 -0700

    fixed some mistakes

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     a_matrix = [[1]] * len(matrix)
     an_index = 0
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace0
         a_list = my_list[:]
         a_list = [x if x != search else replace for x in my_list]
-        return(new_list)
+        return(a_list)
