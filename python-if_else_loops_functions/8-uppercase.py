6f19c3458c49b320335c21d710da02548a5703d6
root <p.savadogo@alustudent.com>
2024-06-28 04:24:12 -0700

    whitespace erroe on 8-uppercas

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str): 
+def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
             newA = ord(str[i]) + (ord('A') - ord('a'))
