ec094fa984cfe66dbcf1adbd15338a8c7213e653
root <p.savadogo@alustudent.com>
2024-06-28 04:21:07 -0700

    8-uppercas finally works

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str): 
-    for iterator in str:
-        temp = iterator
-        if ord(temp) >= 97 and ord(temp) <= 122:
-            temp = chr(ord(iterator) - 32)
-    print("{}".format(temp), end='')
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            newA = ord(str[i]) + (ord('A') - ord('a'))
+        else:
+            newA = ord(str[i])
+        print("{}".format(chr(newA)), end='')
     print()
