9810eab35abac90038446f4dd8a45928e48ebdf4
root <p.savadogo@alustudent.com>
2024-06-28 04:55:06 -0700

    elif example for discussion forum one

---
+++ python-exercises/README.md
@@ -0,0 +1,1 @@
+This directory was created to try solving some python problems on my own to understand better.
---
+++ python-exercises/discussion_forum_1-elif_ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+ 
+#if-elif-else statements
+
+fruit = 'orange'
+
+if fruit == 'apple':
+    print('This fruit is an appple')
+elif fruit == 'orange':
+    print('This fruit is an orange')
+elif fruit == 'grape':
+    print('This fruit is a grape')
+else:
+    print(" This fruit isn't an apple, an orange or a grape")
