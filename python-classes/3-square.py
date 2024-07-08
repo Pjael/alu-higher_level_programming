6ddc3fd0562afb1e624ca7a45575cafff5bbb904
root <p.savadogo@alustudent.com>
2024-07-08 14:27:01 -0700

    3 done

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this is class that defines a square with a private instance attribut size"""
+
+
+class Square:
+
+    """method that defines the class and instantiates size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+    def area(self):
+        """calculates the current square and returns it"""
+        return(self.__size * self.__size)
