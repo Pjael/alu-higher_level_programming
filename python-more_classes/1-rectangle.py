6e1b7a922c1e655ade2a30d04a4af339aa70bc22
root <p.savadogo@alustudent.com>
2024-07-09 13:40:59 -0700

    test again

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class defines rectangle with private instance attribute width"""
+"""class defines rectangle with private instance attribute width and height"""
 
 
 class Rectangle:
