9d4ff83541375c7dc3ff82f8ed81943cfc563957
root <p.savadogo@alustudent.com>
2024-07-09 14:02:11 -0700

    1 thank u H.S

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,9 +18,9 @@ class Rectangle:
         def width(self, value):
             self.__width = value
             if type(value) is not int:
-                raise TypeError(width must be an integer)
+                raise TypeError('width must be an integer')
             if value < 0:
-                raise ValueError(width must be >= 0)
+                raise ValueError('width must be >= 0')
 
         """retrieves private instance attribute height"""
         @property
@@ -32,6 +32,6 @@ class Rectangle:
         def height(self, value):
             self.__height = value
             if type(value) is not int:
-                raise TypeError(heightmust be an integer)
+                raise TypeError('height must be an integer')
             if value < 0:
-                raise ValueError(height must be >= 0)
+                raise ValueError('height must be >= 0')
