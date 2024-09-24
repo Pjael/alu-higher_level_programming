ec390cb82d9aab7b871499ad0923802ac4bd3d88
Pjael <p.savadog@alustudent.com>
2024-09-24 11:12:14 -0700

    2 trying

---
+++ javascript-warm_up/2-argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (args = 0){ 
+	console.log("No argument found");
+}
+else (args = 1){
+	console.log("Argument found");
+}
+if (args > 1){
+	console.log("Arguments found");
+}
