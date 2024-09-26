649d4793cf5ef26ece2fe68673fdd4de156d260b
Pjael <p.savadog@alustudent.com>
2024-09-26 04:56:05 -0700

    4 done

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
-let arg
-console.log( arg1 'is' arg2 )
+const args = process.argv.slice(2);
+
+let Arg1
+let Arg2
+
+if (args[0] !== undefined) {
+	Arg1 = args[0];
+} else {
+	Arg1 = 'undefined'
+} 
+
+if (args[0] !== undefined) {
+	Arg2 = args[1];
+} else {
+	Arg2 = "undefined"
+}
+
+console.log(Arg1 +" "+ "is" +" "+ Arg2);
