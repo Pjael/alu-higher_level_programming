d808ba8ec6ad43cd8a338b47300ad46c78a30238
Pjael <p.savadog@alustudent.com>
2024-09-26 05:59:52 -0700

    10 done

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-const args = process.argv
+const factorial = (n) => {
+	if (n < 0) return 1;
+	if (n === 0 || n === 1) return 1;
+	return n * factorial(n -1);
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(isNaN(num) ? 1 : num));
