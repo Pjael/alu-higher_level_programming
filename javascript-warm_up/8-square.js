1444171d2018ca266dc2f812ef97189487360e27
Pjael <p.savadog@alustudent.com>
2024-09-26 05:49:39 -0700

    8 done

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-const args = process.argv
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+	console.log("Missing size");
+} else {
+	for (let i = 0; i < size; i++) {
+		console.log("X".repeat(size));
+	}
+}
