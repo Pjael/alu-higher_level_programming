4ebf51f53a0b578931cdda7b5b32151b83ec46c7
Pjael <p.savadog@alustudent.com>
2024-09-26 05:37:00 -0700

    11 done

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
-const args = process.argv
+const args = process.argv.slice(2);
+const numbers = args.map(Number);
+
+if (numbers.length < 2) {
+	console.log(0);
+} else {
+	const uniqueNumbers = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (uniqueNumbers.indexOf(numbers[i]) === -1) {
+			uniqueNumbers.push(numbers[i]);
+		}
+}
+	uniqueNumbers.sort((a, b) => b - a);
+
+	console.log(uniqueNumbers[1]);
+}
