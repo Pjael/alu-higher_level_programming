ae130e5ac7127c72cee5e4e16141e9a6254a6588
Pjael <p.savadog@alustudent.com>
2024-10-05 11:18:31 -0700

    5 again

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,18 +7,18 @@ const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
     if (error) {
-        console.error(error);
+        console.error('Error fetching the URL:', error);
         return;
     }
 
     if (response.statusCode !== 200) {
-        console.log(`Failed to retrieve the webpage. Status code: ${response.statusCode}`);
+        console.error('Failed to retrieve the webpage. Status code:', response.statusCode);
         return;
     }
 
     fs.writeFile(filePath, body, 'utf8', (err) => {
         if (err) {
-            console.error(err);
+            console.error('Error writing to file:', err);
             return;
         }
         console.log(`Content saved to ${filePath}`);
