0bed76c1e9c32ef0af1d32010d7a811c087299cd
Pegdwende Savadogo <p.savadogo@alustudent.com>
2025-03-20 10:55:21 +0200

    4 again

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, senda GET request to the URL,a and displays the body of the response
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s $1 -H "X-HolbertonSchool-User-Id: 98"
